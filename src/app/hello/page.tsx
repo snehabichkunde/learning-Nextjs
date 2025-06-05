3877680b25b7e4f98ae1d6ae6465de5f63d18d8f
snehabichkunde <bichkundesneha@gmail.com>
2025-06-05 21:49:15 +0530

    learned about the server componend and cliend component and and loading.tsx will only triggreed for the server component

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,4 +1,16 @@
-export default async function page(){
-    await new Promise((resolve)=> setTimeout(resolve, 1000));
-    return <div>Hello World</div>
+"use client";
+
+import { useEffect } from "react";
+
+export default function Page() {
+  useEffect(() => {
+    async function xyz() {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log("Delayed by 1 second");
+    }
+
+    xyz(); // ✅ Call the async function
+  }, []);
+
+  return <div>Hello World</div>;
 }
